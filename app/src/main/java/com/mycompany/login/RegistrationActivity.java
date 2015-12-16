b1579384d66891aa8fa7c0654cb54a56b390ca4f
mobdev007 <pinkydey233@gmail.com>
2015-12-16 16:52:39 +0530

    changes1

---
+++ app/src/main/java/com/mycompany/login/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.mycompany.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mine on 12/14/2015.
+ */
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
